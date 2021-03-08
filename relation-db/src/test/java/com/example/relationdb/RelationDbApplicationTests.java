c2d1dd13a6ee76df8a2007a09e44f6048227ed1e
Tomasz Stalka <tomek.stalka@gmail.com>
2021-03-08 20:01:30 +0100

    Connection to H2 with JDBC template

---
+++ relation-db/src/test/java/com/example/relationdb/RelationDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relationdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
